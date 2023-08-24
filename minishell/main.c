b66dbbda6ccedc6662c533183ce5860e3f4fdb10
Seungwon An <seunan@c2r2s1.42seoul.kr>
2023-08-24 22:55:41 +0900

    why err?

---
+++ minishell/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunan <seunan@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/24 22:42:16 by seunan            #+#    #+#             */
+/*   Updated: 2023/08/24 22:53:57 by seunan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(void)
+{
+	char *str;
+	while(1)
+	{
+		str = readline("Enter the string : ");
+		if (strcmp(str, "exit") == 0)
+		{
+			printf("exit ...\n");
+			break;
+		}
+		else
+			printf("Result : %s\n\n", str);
+		add_history(str);
+		free(str);
+	}
+	return (0);
+}
