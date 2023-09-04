a70ac941f26ba188bac0e59199dc251128616bf2
Seungwon An <seunan@c2r4s4.42seoul.kr>
2023-09-04 17:24:36 +0900

    a

--- minishell/main.c
+++ minishell/main.c
@@ -1,127 +1,37 @@
 #include "minishell.h"
 
-
-char	*gnl_strndup(char *src, int n)
+char	*find_pair(char *str, char quote)
 {
-	char	*dup;
-	int		i;
 
-	dup = (char *)ft_calloc(n + 1 , sizeof(char));
-	if (!dup)
-		return (NULL);
+}
+
+/*
+1. 문자를 만날 때까지 화이트스페이스를 민다.
+2. 화이트스페이스 기준으로 쪼갠 단위 워드를 만들어 연결리스트에 넣는다.
+	2-1. 만약 따옴표를 만나면 같은 따옴표를 만날때까지 모든 문자를 워드 취급한다.
+		2-1-1. 따옴표가 닫힌 다음에 공백이 아니라면 화이트 스페이스를 만날 때 까지 모든 문자를 워드 취급한다.
+	2-2. 만약 파이프를 만나면 리스트 넣고 다음 워드를 만든다.
+	2-3. 만약 리다이렉션을 만나면 리스트 넣고 다음 워드를 만든다.
+
+bash-3.2$ 'l''''''''s' => ls
+bash-3.2$ ""gre"p" "a" => grep a
+bash-3.2$ ""grep a"" => grep a
+*/
+
+
+t_list	*parse(char *str)
+{
+	t_list	*lst;
+	int	i;
+
 	i = 0;
-	while (i < n)
+	while (str[i] != '\0')
 	{
-		dup[i] = src[i];
-		i++;
+		if (ft_isquote(str[i]))
+			find_pair(str + i, str[i]);
+		++i;
 	}
-	return (dup);
-}
-
-int	even(int n)
-{
-	if (n % 2 == 0)
-	{
-		return TRUE;
-	}
-	else
-	{
-		return FALSE;
-	}
-}
-
-char *meet_quote(char *str, int *i, char quote) //띄어쓰기까지 모두 문자 취급
-{
-	int		idx;
-	int		len;
-	char	*result;
-
-	idx = *i;
-	len = 0;
-	while(str[idx++] != quote)
-		len++;
-	result = gnl_strndup(str + (*i), len);
-	*i = idx;
-	return (result);
-}
-
-char *meet_word(char *str, int *i)
-{
-	int		idx;
-	idx = *i;
-	int len = 0;
-	char *result;
-
-	while(!ft_iswhitespace(str[idx]) && !ft_isquote(str[idx]) && str[idx])
-	{
-		idx++;
-		len++;
-	}
-	result = gnl_strndup(str + (*i), len);
-	*i = idx;
-	printf("result in meet_word = %s\n",result);
-	return (result);
-}
-
-char	*organize_quote(char *str)
-{
-	t_list	*head = NULL;
-	t_list	*lst_tmp;
-	int len = 0;
-	char	*tmp;
-	int i = 0;
-	char *result;
-	while(ft_iswhitespace(str[i]))
-		i++;
-	//일단은 쪼개서 합치는 방식.
-	while(str[i])
-	{
-		if(ft_isquote(str[i]))
-		{
-			// printf("meet_quote\n");
-			tmp = meet_quote(str, &i, str[i]);
-			ft_lstadd_back(&head, ft_lstnew(tmp));
-		}
-		else if(ft_iswhitespace(str[i]))
-		{
-			// printf("meet_ws\n");
-			while(ft_iswhitespace(str[i]))
-				i++;
-			tmp = " ";
-			ft_lstadd_back(&head, ft_lstnew(tmp));
-		}
-		else
-		{
-			// printf("meet_word\n");
-			printf("i = %d\n",i);
-			tmp = meet_word(str, &i);
-			ft_lstadd_back(&head, ft_lstnew(tmp));
-		}
-		printf("word = %s\n",tmp);
-	}
-	printf("escape while\n");
-	lst_tmp = head;
-	printf("%s\n",lst_tmp->token);
-	while(lst_tmp)
-	{
-		len += ft_strlen(lst_tmp->token);
-		lst_tmp = lst_tmp->next;
-	}
-	printf("len = %d\n",len);
-	result = ft_calloc(len + 1, sizeof(char));
-	lst_tmp = head;
-	tmp = result;
-	while(lst_tmp)
-	{
-		i = 0;
-		printf("lst_tmp->token = %s\n",lst_tmp->token);
-		strcpy(tmp,lst_tmp->token);
-		i += ft_strlen(lst_tmp->token);
-		lst_tmp = lst_tmp->next;
-		tmp += i;
-	}
-	printf("result  = %s\n",result);
-	return result;
+	return (lst);
 }
 
 int	main(void)
@@ -137,10 +47,9 @@ int	main(void)
 			break ;
 		}
 		printf("%s\n",str);
-		organize_quote(str);
+		parse(str);
 		add_history(str);
 		free(str);
 	}
 	return (0);
 }
-// USER=test | env | grep USER => 환경변수 변경 적용 안되는게 맞나?
